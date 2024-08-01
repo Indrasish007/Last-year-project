360bec7415223c4b3a50a8a7daae08cfd4ae09ad
Indrasish <indrasishadhya770@gmail.com>
2024-08-02 02:35:48 +0530

    new

--- final/app.py
+++ final/app.py
@@ -1,7 +1,10 @@
 import streamlit as st
 if st.button("home"):
     st.switch_page("app.py")
-if st.button("stock"):
+if st.button("Stock price prediction"):
     st.switch_page("pages/stock.py")
-if st.button("whatsapp"):
+if st.button("Whatsapp Chat Analyzer"):
     st.switch_page("pages/whatsapp.py")
+if st.button("Car price"):
+    st.switch_page("pages/car.py")
+
